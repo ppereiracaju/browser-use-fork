efe4147b91a0940fc16326877a4d57a3a6f9364a
ppereiracaju <93482183+ppereiracaju@users.noreply.github.com>
2025-06-14 18:28:20 -0300

    Update simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -1,25 +1,43 @@
 import asyncio
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from dotenv import load_dotenv
-
-load_dotenv()
-
 from langchain_openai import ChatOpenAI
 
 from browser_use import Agent
+from browser_use.browser import Browser
+
+load_dotenv()
 
 # Initialize the model
 llm = ChatOpenAI(
 	model='gpt-4o',
 	temperature=0.0,
 )
-task = 'Go to kayak.com and find the cheapest one-way flight from Zurich to San Francisco in 3 weeks.'
-agent = Agent(task=task, llm=llm)
+#task = 'Find the founders of browser-use and draft them a short personalized message'
+task="""
+	-Abra o site https://www.serasa.com.br/
+	-selecione "Para empresas"
+	-selecione Acessar
+	-insira login: 37347899
+	-Digite a senha: Condo@24
+	-Click Acessar
+	-Feche o popup (se existir)
+	-Acessar: Serasa Relatório Básico PJ
+	-Insira o CNPJ: 18.710.080/0001-34
+	-Selecionar no dropdown: "Relatório Avançado"
+	-Click: Gerar relatório
+	-No popup Conheça a situação dos sócios, clique em "Sim quero incluir"
+	-Salvar os dados da página
+	-Encerrar
+"""
 
+browser_session = BrowserSession()
+
+agent = Agent(
+	task=task,
+	llm=llm,
+	browser_session=browser_session,
+)
 
 async def main():
 	await agent.run()
